463a979efdbb39f2917fd62f50ff0d98991aea5e
krizzly <seankellyhimself@gmail.com>
2023-05-25 09:31:12 -0400

    Update install scripts

--- oobabooga_install.bat
+++ oobabooga_install.bat
@@ -1,3 +1,4 @@
+:: Note: cl (MSVC C/CPP compiler) and nvcc (Nvidia cuda C/CPP compiler) must be available on system path
 @echo off
 set "venv_name=.venv"
 if exist "%venv_name%\Scripts\python.exe" (
@@ -9,8 +10,18 @@ if exist "%venv_name%\Scripts\python.exe" (
 echo Activating virtual environment...
 call "%venv_name%\Scripts\activate"
 
+:: pip install torch torchvision torchaudio -f https://download.pytorch.org/whl/torch_stable.html
 pip install flask_cloudflared
+pip install https://github.com/jllllll/bitsandbytes-windows-webui/raw/main/bitsandbytes-0.38.1-py3-none-any.whl
+pip install torch==2.0.0+cu118 torchvision torchaudio -f https://download.pytorch.org/whl/torch_stable.html
+pip install ninja gitpython
 pip install -r .\requirements.txt
 pip install -r .\extensions\elevenlabs_tts\requirements.txt
+pip install -r .\extensions\whisper_stt\requirements.txt
+
+md .\repositories
+git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git .\repositories\GPTQ-for-LLaMa
+cd .\repositories\GPTQ-for-LLaMa
+python .\setup_cuda.py install
 
 echo Successfully installed API, ready to start using oobabooga_run.bat
--- oobabooga_install.sh
+++ oobabooga_install.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
+# Note: gcc (GNU C/CPP compiler) and nvcc (Nvidia cuda C/CPP compiler) must be available on system path
+
 venv_name=".venv"
-if [ -d "$venv_name" ]; then
-    echo "Virtual environment '$venv_name' already exists."
+if [ -f "${venv_name}/bin/python" ]; then
+    echo "Virtual environment '${venv_name}' already exists."
 else
-    echo "Creating virtual environment '$venv_name'..."
-    python3 -m venv "$venv_name"
+    echo "Creating virtual environment '${venv_name}'..."
+    python3 -m venv "${venv_name}"
 fi
 echo "Activating virtual environment..."
-source "$venv_name/bin/activate"
+source "${venv_name}/bin/activate"
 
-pip install -U flask_cloudflared
+# pip install torch torchvision torchaudio -f https://download.pytorch.org/whl/torch_stable.html
+pip install flask_cloudflared
+pip install https://github.com/jllllll/bitsandbytes-windows-webui/raw/main/bitsandbytes-0.38.1-py3-none-any.whl
+pip install torch==2.0.0+cu118 torchvision torchaudio -f https://download.pytorch.org/whl/torch_stable.html
+pip install ninja gitpython
 pip install -r ./requirements.txt
 pip install -r ./extensions/elevenlabs_tts/requirements.txt
+pip install -r ./extensions/whisper_stt/requirements.txt
+
+mkdir -p ./repositories
+git clone https://github.com/oobabooga/GPTQ-for-LLaMa.git ./repositories/GPTQ-for-LLaMa
+cd ./repositories/GPTQ-for-LLaMa
+python3 ./setup_cuda.py install
 
 echo "Successfully installed API, ready to start using oobabooga_run.sh"
